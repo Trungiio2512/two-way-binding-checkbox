8bf4abb437a1d984a3f32c9b3ca1f454dff5dd74
Trungiio <91168572+Trungiio2512@users.noreply.github.com>
2022-07-05 21:14:37 +0700

    checked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import { useState } from "react";
 import "./styles.css";
 
+const courses = [
+  {
+    id: 1,
+    name: "Html-Css"
+  },
+  {
+    id: 2,
+    name: "ReactJs"
+  },
+  {
+    id: 3,
+    name: "Javascript"
+  }
+];
+
 export default function App() {
+  const [course, setCourse] = useState([]);
+
+  const hanldeData = (id) => {
+    const isChecked = course.includes(id);
+
+    if (isChecked) {
+      setCourse((prev) => prev.filter((item) => item !== id));
+    } else {
+      setCourse((prev) => {
+        return [...prev, id];
+      });
+    }
+  };
+
+  const hanldeSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {courses.map((item) => {
+        return (
+          <div key={item.id}>
+            <input
+              type="checkbox"
+              checked={course.includes(item.id)}
+              onChange={() => hanldeData(item.id)}
+            />
+            {item.name}
+          </div>
+        );
+      })}
+      <button onClick={() => hanldeSubmit(course)}>submit</button>
     </div>
   );
 }
